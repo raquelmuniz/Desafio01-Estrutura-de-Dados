2beca999388785dee8809ab43d43e7a637421fc9
raquelmuniz <munizraquel70@gmail.com>
2022-06-09 17:56:43 -0300

    primeiro desafio script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const name = "raquel";
+console.log("name", name);
+
+const number = 19;
+console.log("number", number);
+
+const year = 2022;
+console.log("year", year);
+
+const subtraction = year - number;
+console.log("subtraction", subtraction);
+
+console.log("Estou mexendo no console.log");
+
+const quartaFeira = true;
+console.log("quartaFeira", quartaFeira);
+
+console.log("typeof", typeof quartaFeira);
